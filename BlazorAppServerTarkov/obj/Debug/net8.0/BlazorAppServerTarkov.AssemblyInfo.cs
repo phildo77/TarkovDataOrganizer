0514aed256f495e85838a62ebfa76a85f2c3ecf6
Aidan Gignac <gignacaidan@yahoo.com>
2024-07-03 16:48:44 -0600

    Added Horizontal/Vertical Recoil

--- BlazorAppServerTarkov/obj/Debug/net8.0/BlazorAppServerTarkov.AssemblyInfo.cs
+++ BlazorAppServerTarkov/obj/Debug/net8.0/BlazorAppServerTarkov.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorAppServerTarkov")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72b69e9e091060d7b86fdf4b13021f4f8563ca47")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb8aabe47de68ea3aa1e50ded85997869aa9c384")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorAppServerTarkov")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorAppServerTarkov")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
