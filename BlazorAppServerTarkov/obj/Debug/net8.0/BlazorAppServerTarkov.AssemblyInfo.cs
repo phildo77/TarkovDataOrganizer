7630fa641c88b009ed828ed5dee37e5d3fa74cd6
Aidan Gignac <gignacaidan@yahoo.com>
2024-07-16 16:56:21 -0600

    Added MudBlazor project reference

--- BlazorAppServerTarkov/obj/Debug/net8.0/BlazorAppServerTarkov.AssemblyInfo.cs
+++ BlazorAppServerTarkov/obj/Debug/net8.0/BlazorAppServerTarkov.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorAppServerTarkov")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0634b73528f6c19d0c5595792fc9360ce412f17a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef265c5adaff871b9a0b8a9e5087306d26d8eb0f")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorAppServerTarkov")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorAppServerTarkov")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
