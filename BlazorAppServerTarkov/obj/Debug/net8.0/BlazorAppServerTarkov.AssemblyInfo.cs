f8cc97fe1d5d56ebbc67907a7f6cf4e05daae779
Aidan Gignac <gignacaidan@yahoo.com>
2024-08-07 13:54:22 -0600

    Updated Weapon Display Logic and Razor

--- BlazorAppServerTarkov/obj/Debug/net8.0/BlazorAppServerTarkov.AssemblyInfo.cs
+++ BlazorAppServerTarkov/obj/Debug/net8.0/BlazorAppServerTarkov.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorAppServerTarkov")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b535dd09293bc2a5f570e140a9f807853be6c5cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22afc976427ad5bad69451f26b2ced2b402189f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorAppServerTarkov")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorAppServerTarkov")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
