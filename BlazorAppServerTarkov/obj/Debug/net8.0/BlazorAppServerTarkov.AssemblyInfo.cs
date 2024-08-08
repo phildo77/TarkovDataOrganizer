8f9c411d43b6588e0e193f9f47cd0f68a0c181a6
Aidan Gignac <gignacaidan@yahoo.com>
2024-08-08 11:38:21 -0700

    Updated Filters

--- BlazorAppServerTarkov/obj/Debug/net8.0/BlazorAppServerTarkov.AssemblyInfo.cs
+++ BlazorAppServerTarkov/obj/Debug/net8.0/BlazorAppServerTarkov.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorAppServerTarkov")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d07e14864531010d047cc0586c096e0889a0e98")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3095f7aedad55dcf94728b141130cb26beb32e1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorAppServerTarkov")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorAppServerTarkov")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
