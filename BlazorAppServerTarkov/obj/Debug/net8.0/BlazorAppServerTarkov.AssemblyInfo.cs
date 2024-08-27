d3a2d5699fb573504094ba769fed73ee25c6e2dd
Aidan Gignac <gignacaidan@yahoo.com>
2024-08-27 10:36:17 -0700

    Added Drawer for Selected Combination

--- BlazorAppServerTarkov/obj/Debug/net8.0/BlazorAppServerTarkov.AssemblyInfo.cs
+++ BlazorAppServerTarkov/obj/Debug/net8.0/BlazorAppServerTarkov.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorAppServerTarkov")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a5cdd67321dbc14d7fcf2b047fae55844718b9c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66325387617fb4673224a9bbe48405be28867bdc")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorAppServerTarkov")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorAppServerTarkov")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
