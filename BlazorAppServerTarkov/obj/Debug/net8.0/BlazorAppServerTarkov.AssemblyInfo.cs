cfd9315f46882213900614695848f2cc9ed43124
Aidan Gignac <gignacaidan@yahoo.com>
2024-09-04 14:05:09 -0600

    Remove Strong Node

--- BlazorAppServerTarkov/obj/Debug/net8.0/BlazorAppServerTarkov.AssemblyInfo.cs
+++ BlazorAppServerTarkov/obj/Debug/net8.0/BlazorAppServerTarkov.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorAppServerTarkov")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3c439a7ddf2a5e3b5c7cd2f11be777e653f78fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+587e072d9b3856eff5fa1abbb0b351e5a5ff2254")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorAppServerTarkov")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorAppServerTarkov")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
