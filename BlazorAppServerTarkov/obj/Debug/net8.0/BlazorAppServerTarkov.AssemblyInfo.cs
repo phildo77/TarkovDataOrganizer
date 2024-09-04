9d07e52186f971618034e1fc7ec382256844df47
Aidan Gignac <gignacaidan@yahoo.com>
2024-09-04 17:04:11 -0600

    Added addtional filtering logic

--- BlazorAppServerTarkov/obj/Debug/net8.0/BlazorAppServerTarkov.AssemblyInfo.cs
+++ BlazorAppServerTarkov/obj/Debug/net8.0/BlazorAppServerTarkov.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorAppServerTarkov")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ffd81a97d281a0ec4ad58b1fe6ebb405b748992")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c23df5aea6eda148b671fb272f1834988b3c6be9")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorAppServerTarkov")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorAppServerTarkov")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
